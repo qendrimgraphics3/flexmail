ad1ffbca93e5b88e3dad2adff9ebcaf4eaad7524
qendrimgraphics3 <qendrimgraphics3@gmail.com>
2024-11-14 16:53:22 +0100

    add

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px', marginBottom: '10px' }}>
+            <div>
+                <h4>by <a href='https://github.com/qendrimgraphics3' target={`_blank`} style={{ textDecoration: 'none' }}>Qendrim Graphics</a> </h4>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
